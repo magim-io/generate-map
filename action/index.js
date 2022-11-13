ec10ac1f723194599ebfdeed5a89920923d2c3a5
TonyInfinity <quachhengtony@gmail.com>
2022-11-13 19:00:40 +0700

    add override default config

--- action/index.js
+++ action/index.js
@@ -155,7 +155,7 @@ function executePackage(packageName) {
                         })];
                 case "typescript-eslint":
                     return [2 /*return*/, new Promise(function (resolve, reject) {
-                            exec("npx eslint . -o ./magim-scamap.json", {
+                            exec("npx eslint . -c ./magim-scamap.config.cjs -o ./magim-scamap.json", {
                                 cwd: "server"
                             }, function (error, stdout) {
                                 if (error) {
